c2db3601d9e0119313f4ff545f637603aa5f2413
Yasmeen Wafa <you@example.com>
2018-12-17 11:53:55 +0200

    started products page

--- page-templates/products.php
+++ page-templates/products.php
@@ -4,5 +4,9 @@ Template Name: Products
 */
 get_header(); ?>
 
-<h1>Products</h1>
+<div class="products-page-container">
+  <h1>We Desvise and
+Launch Digital
+Products</h1>
+</div>
 <?php get_footer();
