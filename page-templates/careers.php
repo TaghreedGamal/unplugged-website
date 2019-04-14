f713562020b0b00b9bc5d97f26cd7cea9cada0dd
Yasmeen Wafa <you@example.com>
2019-04-14 16:55:55 +0200

    started careers page

---
+++ page-templates/careers.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Careers
+*/
+get_header(); ?>
+<div class="careers-page-container">
+  <div class="products-page-container-header" style="background-image: url(<?php echo get_stylesheet_directory_uri();?>/src/assets/images/products/background.png);" >
+
+    <h1 class="products-header">Coffee is <br> our bestfriend
+  </h1>
+  <div class="header-images">
+    <!-- <img class="empty-rocket" src="<?php echo get_stylesheet_directory_uri()?>/src/assets/images/products/rocket-wireframe.png" alt="">
+    <img class="middle-rocket"src="<?php echo get_stylesheet_directory_uri()?>/src/assets/images/products/middle-rocket-2.png" alt="">
+    <img class="last-rocket"src="<?php echo get_stylesheet_directory_uri()?>/src/assets/images/products/rocket-toy.png" alt=""> -->
+  </div>
+  </div>
+</div>
+<?php get_footer(); ?>
