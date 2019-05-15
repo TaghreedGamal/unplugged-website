5aa259201e1b63e8bd9861a84eb60a55f21e2863
Yasmeen Wafa <you@example.com>
2019-05-15 15:43:13 +0200

    gravity form for contact form

--- page-templates/contact-form-pop-up.php
+++ page-templates/contact-form-pop-up.php
@@ -1,5 +1,7 @@
 <div class="contact-form tiny reveal" id="contact-form-modal" data-reveal>
-  <div class="pipedriveWebForms" data-pd-webforms="https://pipedrivewebforms.com/form/0a564be458cd460cdf4c1240b036f580400659"><script src="https://cdn.pipedriveassets.com/web-form-assets/webforms.min.js"></script></div>
+  <!-- <div class="pipedriveWebForms" data-pd-webforms="https://pipedrivewebforms.com/form/0a564be458cd460cdf4c1240b036f580400659"><script src="https://cdn.pipedriveassets.com/web-form-assets/webforms.min.js"></script></div> -->
+  <?php echo do_shortcode('[gravityform id="8" title ="false" ajax="true"]');?>
+
   <button class="close-button" data-close aria-label="Close modal" type="button">
   <span aria-hidden="true">&times;</span>
 </button>
